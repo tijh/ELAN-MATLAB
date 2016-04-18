c99c6f27d5b909c3c662c5e7b1401872f4402dc6
tijh <tijh@users.noreply.github.com>
2016-04-18 18:21:42 +0300

    Delete elanAudio.m

--- elanAudio.m
+++
@@ -1,8 +0,0 @@
-function elan = elanAudio(file, attribs)
-
-% function to read in an audio file, detect silence, speech and music, and
-% write to elan annotation file format 
-%
-%
-% Last edit: TH 30.12. 2014
-
